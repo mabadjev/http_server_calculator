a56217da4c226141eb0d6aa9300be8b3ce018a5d
Maksim Abadjev <maks.abadjev@gmail.com>
2018-08-19 17:12:58 -0400

    Calc passes bad input test

--- http_calc_server_test.go
+++ http_calc_server_test.go
@@ -27,3 +27,25 @@ func TestMissing(t *testing.T) {
 	}
 
 }
+
+func TestBadInput(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/add?x=a&y=2", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handleCall(AddFunc))
+
+	handler.ServeHTTP(rr, req)
+
+	expected := `{"action":"add","x":0,"y":0,"answer":0,"cached":false,"error":"Malformed calculator request"}`
+
+	if rr.Body.String() != expected {
+
+		t.Errorf("Did not receive expected result: got %s expected %s", rr.Body.String(), expected)
+
+	}
+
+}
