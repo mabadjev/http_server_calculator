3c099c26974ca1ce23a31eec03db2c9ade8edf70
Maksim Abadjev <maks.abadjev@gmail.com>
2018-08-19 19:53:12 -0400

    Added tests for caching structure. All tests pass.

--- http_calc_server_test.go
+++ http_calc_server_test.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestMissing(t *testing.T) {
@@ -151,6 +152,68 @@ func TestCalculatorDivide(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	expected := `{"action":"divide","x":6,"y":2,"answer":3,"cached":false}`
+
+	if rr.Body.String() != expected {
+
+		t.Errorf("Did not receive expected result: got %s expected %s", rr.Body.String(), expected)
+
+	}
+
+}
+
+func TestCacheSimple(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/divide?x=8&y=2", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handleCall(DiviFunc))
+
+	handler.ServeHTTP(rr, req)
+
+	handler.ServeHTTP(rr, req)
+
+	expected1 := `{"action":"divide","x":8,"y":2,"answer":4,"cached":false}`
+
+	expected2 := `{"action":"divide","x":8,"y":2,"answer":4,"cached":true}`
+
+	expected := expected1 + expected2
+
+	if rr.Body.String() != expected {
+
+		t.Errorf("Did not receive expected result: got %s expected %s", rr.Body.String(), expected)
+
+	}
+
+}
+
+func TestCacheExpire(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/divide?x=9&y=3", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handleCall(DiviFunc))
+
+	handler.ServeHTTP(rr, req)
+
+	expected1 := `{"action":"divide","x":9,"y":3,"answer":3,"cached":false}`
+
+	time.Sleep(time.Minute+time.Second*5)
+	handler.ServeHTTP(rr, req)
+
+	expected2 := `{"action":"divide","x":9,"y":3,"answer":3,"cached":false}`
+
+	time.Sleep(time.Second*5)
+	handler.ServeHTTP(rr, req)
+
+	expected3 := `{"action":"divide","x":9,"y":3,"answer":3,"cached":true}`
+
+	expected := expected1 + expected2+expected3
 
 	if rr.Body.String() != expected {
 
